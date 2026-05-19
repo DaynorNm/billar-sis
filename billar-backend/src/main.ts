dc70d59ced89324eb7c5009442c9fafb0050e352
DaynorNm <daynor.mamani@uab.edu.bo>
2026-05-19 16:58:35 -0400

    chore: modificamos archivo main del backend para servidor

--- billar-backend/src/main.ts
+++ billar-backend/src/main.ts
@@ -6,12 +6,13 @@ async function bootstrap() {
   
   // Habilitamos CORS para que tu Angular (frontend) pueda hacerle peticiones sin bloqueos de seguridad
   app.enableCors({
-    origin: '*', // Permite que cualquier dispositivo de la red consuma la API
+    origin: '*', // Permite que cualquier IP consuma la API
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 Servidor del billar corriendo en: http://localhost:3000`);
+  await app.listen(3000, '0.0.0.0');
+  console.log('🚀 Servidor del billar corriendo en la red local puerto 3000');
 }
 bootstrap();
