dcfbb508caaaf9209ecf86d9096e1a2935337915
DaynorNm <daynor.mamani@uab.edu.bo>
2026-05-19 03:34:08 -0400

    feat: modificacion de permisos de endpoints

--- billar-backend/src/main.ts
+++ billar-backend/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Habilitamos CORS para que tu Angular (frontend) pueda hacerle peticiones sin bloqueos de seguridad
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Permite que cualquier dispositivo de la red consuma la API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`🚀 Servidor del billar corriendo en: http://localhost:3000`);
